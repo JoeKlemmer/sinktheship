d26102cb8534f1c7fefda6ea380a890d6135a66d
Joe Klemmer <joe.klemmer@gmail.com>
2020-05-03 22:38:48 -0400

    Added way to much for this to be a second commit (do better Joe)

--- sinktheship.py
+++ sinktheship.py
@@ -1,15 +1,18 @@
 import random
+import os
 
 random.seed()
 
 # Variables
 
-shipLocation = 0
 acrossCoord = 0
 downCoord = 0
 playingTheGame = True
 choice = 0
 gridSize = 0
+
+shipLocation = [[int(j) for j in range(35)] for i in range(35)]
+playingGrid = [[int(j) for j in range(35)] for i in range(35)]
 
 displayHelpInfo = """
 The object of the game is to sink the ship.  There will be one ship 
@@ -25,11 +28,19 @@ will be set as so; "
 You may exit the game at any time by hitting \"Ctrl-C\".
 """
 
-# Functions
+# Pause Function
 def pause():
     pause = input("Press any key to continue...")
     
+# Build a 2 dimentional array of the size chosen
 def buildGrid(gSize):
+    for i in range(gSize):
+        for j in range(gSize):
+            playingGrid[i][j] = "  ."
+                        
+# Display the playing grid the size chosen
+def displayGrid(gSize):
+    os.system("clear")
     for x in range(gSize):
         if x < 10:
             print("  " + str(x+1), end="")
@@ -47,10 +58,40 @@ def buildGrid(gSize):
 
     for i in range(gSize):
         for j in range(gSize):
-            print("  .", end="")
+            print(playingGrid[i][j], end="")
         print(" |" + str(i+1))
 
-# Main game loop
+def getUsersCoordinates(gSize):
+#    hity = random.randint(1, gridSize - 1)
+
+    acrossCoord = int(input("Enter across coordinate: "))
+    downCoord = int(input("Enter down coordinate: "))
+
+    playingGrid[downCoord - 1][acrossCoord - 1] = "  X"
+
+def playGame(gSize):
+
+    isItSunk = 0
+    playCounter = 0
+
+    buildGrid(gSize)
+    # placeShip()
+    displayGrid(gSize)
+
+    while isItSunk < 4:
+        playCounter += 1
+
+        getUsersCoordinates(gSize)
+        
+        isItSunk += 1
+
+        displayGrid(gSize)
+
+    print("\n\"Hey! You sunk my ship!\"")
+    print("\"It took you " + str(playCounter) + " shots to sink it.\"")
+
+
+# Initial game menu
 while playingTheGame:
     print("Please make a selection:")
     print("1 - Easy")
@@ -63,14 +104,14 @@ while playingTheGame:
     choice = input("Enter your choice: ")
 
     if str(choice) == "1":
-#        print("Easy")
-        gridSize = 10
+        playGame(10)
+        break
     elif str(choice) == "2":
-#        print("Normal")
-        gridSize = 15
+        playGame(15)
+        break
     elif str(choice) == "3":
-#        print("Hard")
-        gridSize = 20
+        playGame(20)
+        break
     elif str(choice) == "H" or str(choice) == "h":
         print(displayHelpInfo)
         gridSize = 0
@@ -82,11 +123,6 @@ while playingTheGame:
         break
     else:
         print("Error")
-
-    buildGrid(gridSize)
-
-    print("Enter across coordinate")
-    print("Enter down coordinate")
 
     pause()
 

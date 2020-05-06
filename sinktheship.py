824fd0260eaef7c1681db37653c81c6867d42b7c
Joe Klemmer <joe.klemmer@gmail.com>
2020-05-06 18:18:24 -0400

    Added a limit on how many attempts the player can make; did more cleaning

--- sinktheship.py
+++ sinktheship.py
@@ -5,40 +5,37 @@ random.seed()
 
 # Variables
 
-playingTheGame = True
-choice = 0
-gridsize = 0
+playing_the_game = True
 
-shipLocation = [[int(j) for j in range(35)] for _ in range(35)]
-playingGrid = [[int(j) for j in range(35)] for _ in range(35)]
+ship_location = [[int(j) for j in range(25)] for _ in range(25)]
+playing_grid = [[int(j) for j in range(25)] for _ in range(25)]
 
-displayHelpInfo = """
+display_help_info = """
 The object of the game is to sink the ship.  There will be one ship 
 on the grid.  The player will choose horizontal and virtical coordinates 
 and the game will calculate whether those coordiantes were a hit or a 
 miss.  An \"X\" will mark a hit and \"*\" a miss.  The difficulty levels 
-will be set as so; "
+will be set as follows; "
 
-\tEasy will be a grid of 10 x 10
-\tNormal will be a grid of 15 x 15
-\tHard will be a grid of 20 x 20
+ - Easy will be a grid of 10 x 10
+ - Normal will be a grid of 15 x 15
+ - Hard will be a grid of 20 x 20
+
+You will have 15, 20, and 25 attempts, respectively, to sink the ship.
+If you do not, the ship will escape and you lose.
 
 You may exit the game at any time by hitting \"Ctrl-C\".
 """
 
-# Pause Function
-def pause():
-    pause = input("Press any key to continue...")
-    
 # Build a 2 dimensional array of the size chosen
 def build_grid(gsize):
     for i in range(gsize):
         for j in range(gsize):
-            playingGrid[i][j] = "  ."
+            playing_grid[i][j] = "  ."
                         
 # Display the playing grid the size chosen
 def display_grid(gsize):
-    # os.system("clear")
+    #os.system("clear")
     for x in range(gsize):
         if x < 10:
             print("  " + str(x+1), end="")
@@ -57,9 +54,10 @@ def display_grid(gsize):
 
     for i in range(gsize):
         for j in range(gsize):
-            print(playingGrid[i][j], end="")
+            print(playing_grid[i][j], end="")
         print(" |" + str(i+1))
 
+# Get the users coordinates
 def get_users_coordinates(gsize):
     while True:
         try:
@@ -94,21 +92,17 @@ def place_ship(gsize):
 
     while i < 4:
         if direction == 1:  # North
-            print("N" + str(limiter+i) + "x" + str(gridsize))
-            playingGrid[limiter+i][gridsize] = "  @"
-            shipLocation[limiter+i][gridsize] = "  @"
+#            playing_grid[limiter+i][gridsize] = "  @"
+            ship_location[limiter+i][gridsize] = "  @"
         elif direction == 2:    #South
-            print("S" + str(limiter + 3) - i + "x" + str(gridsize))
-            playingGrid[(limiter + 3) - i][gridsize] = "  @"
-            shipLocation[(limiter + 3) - i][gridsize] = "  @"
+#            playing_grid[(limiter + 3) - i][gridsize] = "  @"
+            ship_location[(limiter + 3) - i][gridsize] = "  @"
         elif direction == 3:    #East
-            print("E" + str(gridsize) + "x" + str((limiter + 3) - i))
-            playingGrid[gridsize][(limiter + 3) - i] = "  @"
-            shipLocation[gridsize][(limiter + 3) - i] = "  @"
+#            playing_grid[gridsize][(limiter + 3) - i] = "  @"
+            ship_location[gridsize][(limiter + 3) - i] = "  @"
         elif direction == 4:    #West
-            print("W" + str(gridsize) + "x" + str(limiter + i))
-            playingGrid[gridsize][limiter + i] = "  @"
-            shipLocation[gridsize][limiter + i] = "  @"
+#            playing_grid[gridsize][limiter + i] = "  @"
+            ship_location[gridsize][limiter + i] = "  @"
         else:
             print("Shit done did blowed up!")
         i += 1
@@ -116,45 +110,47 @@ def place_ship(gsize):
 def calculate_hit():
     hit_counter = 0
 
-    print("P: ", end="")
-    print(playingGrid[downCoord-1][acrossCoord-1])
-
-    print("S: ", end="")
-    print(shipLocation[downCoord-1][acrossCoord-1])
-
-    if playingGrid[downCoord - 1][acrossCoord - 1] == shipLocation[downCoord - 1][acrossCoord - 1]:
-        playingGrid[downCoord-1][acrossCoord-1] = "  X"
+    if playing_grid[downCoord - 1][acrossCoord - 1] == ship_location[downCoord - 1][acrossCoord - 1]:
+        playing_grid[downCoord-1][acrossCoord-1] = "  X"
         hit_counter += 1
     else:
-        playingGrid[downCoord-1][acrossCoord-1] = "  *"
+        playing_grid[downCoord-1][acrossCoord-1] = "  *"
 
     return hit_counter
 
 def play_game(gsize):
 
-    is_is_sunk = 0
+    is_it_sunk = 0
     play_counter = 0
 
     build_grid(gsize)
     place_ship(gsize)
     display_grid(gsize)
 
-    while is_is_sunk < 4:
+    while is_it_sunk < 4 and play_counter < gsize:
         play_counter += 1
 
         get_users_coordinates(gsize)
 
-        is_is_sunk += calculate_hit()
+        is_it_sunk += calculate_hit()
 
         display_grid(gsize)
-
-    print("\n\"Hey! You sunk my ship!\"")
-    print("\"It took you " + str(play_counter) + " shots to sink it.\"")
-
+        
+    if is_it_sunk == 4:
+        print("\n\"Hey! You sunk my ship!\"")
+        print("\"It took you " + str(play_counter) + " shots to sink it.\"")
+    elif play_counter == gsize:
+        print("\n\"Ha! You took too long. I got away!\"")
+    else:
+        print("Shit done did blowed up again!")
 
 # Initial game menu
-while playingTheGame:
-    print("Please make a selection:")
+
+print("\nGreetings Professor Falken.")
+print("Welcome to \"Sink My Ship\"")
+
+while playing_the_game:
+    print("\nPlease make a selection:")
     print("1 - Easy")
     print("2 - Normal")
     print("3 - Hard")
@@ -174,16 +170,14 @@ while playingTheGame:
         play_game(20)
         break
     elif str(choice) == "H" or str(choice) == "h":
-        print(displayHelpInfo)
-        gridsize = 0
-        pause()
+        print(display_help_info)
     elif str(choice) == "Q" or str(choice) == "q":
-        playingTheGame = False
+        playing_the_game = False
         print("\n\nThank you for playing.")
         print("Goodbye.\n")
         break
     else:
-        print("Error")
+        print("\nError: You must enter one of the above options\n")
 
-    pause()
+    pause = input("Press any key to continue...")
 
